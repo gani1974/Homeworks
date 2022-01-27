94a5984d89e6778ac19c9c701aaebcb98c2d2514
Gani <ganiiz@mail.ru>
2022-01-27 10:34:26 +0600

    commit 1

--- src/main/java/homework3/searchMissed.java
+++ src/main/java/homework3/searchMissed.java
@@ -5,29 +5,28 @@ import java.util.Arrays;
 public class searchMissed {
 
     public static void main(String[] args) {
-        test(3,1,2,4,5,6,7,8);
+//        test(11, 1, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16);
+//        test(3,1, 2, 4, 5, 6);
+        test(1);
     }
 
     public static int search(int[] arr) {
-        System.out.println(Arrays.toString(arr));
         int start = 0;
         int end = arr.length - 1;
 
         while (start <= end) {
             int base = (start + end) / 2;
-            System.out.println("base: " + base);
             if (arr[base] - base == 1) {
                 start = base + 1;
-                System.out.println("start: " + start);
             } else {
                 end = base - 1;
-                System.out.println("end: " + end);
             }
         }
         return start + 1;
     }
 
     public static void test(int lostNum, int... arr) {
-        System.out.printf("Пропущено: %d, найдено: %d%n", lostNum, search(arr));
+//        System.out.printf("Пропущено: %d, найдено: %d%n", lostNum, search(arr));
+        System.out.println(Arrays.toString(arr) + " => " + lostNum);
     }
 }
